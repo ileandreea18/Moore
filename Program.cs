b9552fbff46c1643bac3576db2086b900f02a2af
ileandreea18 <72399039+ileandreea18@users.noreply.github.com>
2020-10-18 17:29:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HomeworkMoore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Homework_Moore///
+            string consol;
+            double t;
+            while(true)
+            {
+                Console.WriteLine("Introduceti un numar n pentru a afla cate procesoare se pot forma");
+                consol = Console.ReadLine();
+                if (double.TryParse(consol, out t))
+                {
+                    double a;
+                    a = 1.5 * Math.Log(t, 2);
+                    Console.WriteLine("Procesoarele vor fi mai dense de {0} ori in {1} ani", t, a);
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Introduceti un numar n care are mai multe zecimale dupa virgula");
+                    Console.WriteLine("");
+                }
+
+
+
+
+
+            }
+        }
+    }
+}
